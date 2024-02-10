70906a16535adc5d2571cd259e93a702285828d3
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-02-10 12:24:56 +0300

    Create initial test data

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :owner
+
+  enum :brand, [:audi, :bmw, :mercedes, :kia, :hyundai, :toyota, :lexus]
+  enum :engine, [:petrol, :diesel, :electric]
+  enum :transmission, [:mechanical, :automatic, :robotic]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+owners = 10.times do
+  Owner.create(
+    [
+      {
+        name: Faker::Name.first_name,
+        surname: Faker::Name.last_name,
+        age: Faker::Number.between(from: 18, to: 80)
+      }
+    ]
+  )
+end
+
+cars = Owner.all.each do |owner|
+  owner.cars.create([
+    {
+      brand: Car.brands.values.sample,
+      engine: Car.engines.values.sample,
+      transmission: Car.transmissions[:mechanical]
+    },
+    {
+      brand: Car.brands.values.sample,
+      engine: Car.engines.values.sample,
+      transmission: Car.transmissions[:automatic]
+    }
+  ])
+end
